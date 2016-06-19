9dcaabf99565e1726e91576c494f10a9beb6133d
beta <jiangyouxin@taou.com>
2016-06-19 14:55:42 +0800

    缓存js和css

--- app.js
+++ app.js
@@ -3,56 +3,68 @@ var serve = require('koa-static');
 var mount = require('koa-mount');
 var logger = require('koa-logger');
 var conf = require('./conf');
-var app = koa();
+var path = require('path');
 var session = require('koa-generic-session');
 var MongoStore = require('koa-generic-session-mongo');
 var oauth = require('./utility/wechat').oauth;
 var qs = require('querystring');
+var co = require('co');
+var ms = require('ms');
 var gzip = require('koa-gzip');
 var Jade = require('koa-jade')
-var jade = new Jade({
-    viewPath: './front_end/templates',
-    debug: false,
-    pretty: false,
-    compileDebug: false,
-    app: app // equals to jade.use(app) and app.use(jade.middleware)
-})
+var filemd5 = require('md5-file-promise')().computeFromFile;
 
-require('koa-qs')(app);
 require('babel/register')({
     stage: 0
 });
 
-app.keys = [conf.secret];
-app.use(session({
-    store: new MongoStore({
-        url: conf.mongodb
+co.wrap(function *() {
+    var js_md5 = yield filemd5(path.join(__dirname, 'static/scripts/bundle.min.js'));
+    var css_md5 = yield filemd5(path.join(__dirname, 'static/styles/bundle.min.css'));
+    var app = koa();
+    require('koa-qs')(app);
+    var jade = new Jade({
+        viewPath: './front_end/templates',
+        locals: {
+            js_md5: js_md5,
+            css_md5: css_md5
+        },
+        debug: false,
+        pretty: false,
+        compileDebug: false,
+        app: app // equals to jade.use(app) and app.use(jade.middleware)
     })
-}));
-app.use(logger());
-// TODO: 对公众号事件通知的处理
-// TODO: 某现成的统计平台
-// TODO：云主机上不能用ROOT运行
+    app.keys = [conf.secret];
+    app.use(session({
+        store: new MongoStore({
+            url: conf.mongodb
+        })
+    }));
+    app.use(logger());
+    // TODO: 对公众号事件通知的处理
+    // TODO: 某现成的统计平台
+    // TODO：云主机上不能用ROOT运行
 
-app.use(gzip());
-app.use(mount('/agent', function *() {
-    this.body = this.query.echostr;
-}));
-app.use(mount('/internal_login', require('./routes/internal_login')));
-app.use(mount('/login', require('./routes/login')));
-app.use(mount('/static', serve('static')));
-app.use(function *(next) {
-    if (!this.session.user_id) {
-        var origin = conf.site + '/login?' + qs.stringify({
-            target: this.originalUrl,
-            host: this.host
-        });
-        var url = oauth.getAuthorizeURL(origin, 'state', 'snsapi_userinfo');
-        this.redirect(url);
-    } else {
-        yield *next;
-    }
-});
-app.use(mount('/app', require('./routes/app')));
-app.use(mount('/api', require('./routes/api')));
-app.listen(conf.port || 80);
+    app.use(gzip());
+    app.use(mount('/agent', function *() {
+        this.body = this.query.echostr;
+    }));
+    app.use(mount('/internal_login', require('./routes/internal_login')));
+    app.use(mount('/login', require('./routes/login')));
+    app.use(mount('/static', serve('static', {maxAge: ms('10y')})));
+    app.use(function *(next) {
+        if (!this.session.user_id) {
+            var origin = conf.site + '/login?' + qs.stringify({
+                target: this.originalUrl,
+                host: this.host
+            });
+            var url = oauth.getAuthorizeURL(origin, 'state', 'snsapi_userinfo');
+            this.redirect(url);
+        } else {
+            yield *next;
+        }
+    });
+    app.use(mount('/app', require('./routes/app')));
+    app.use(mount('/api', require('./routes/api')));
+    app.listen(conf.port || 80);
+})();
