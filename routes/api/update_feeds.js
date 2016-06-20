a70d955d4768808045b0a3e42a4c17d00e1b2d23
beta <jiangyouxin@taou.com>
2016-06-20 09:29:38 +0800

    重调时间因子

--- routes/api/update_feeds.js
+++ routes/api/update_feeds.js
@@ -24,7 +24,7 @@ function *getNewPostIds(my_id, origin_post_ids, limit) {
     // 按实时性衰减重算rank并排序
     posts = posts.map((post) => {
         // 按（已发的时间 + 若干小时）作为分母来衰减
-        var d = (new Date() - ObjectId(post._id).getTimestamp()) / ms('12h') + 1;
+        var d = (new Date() - ObjectId(post._id).getTimestamp()) / ms('1d') + 1;
         post = post.toObject();
         post.rank = post.rank0 / d;
         return post;
