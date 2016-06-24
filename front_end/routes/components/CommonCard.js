0bec7ea4ce8670d2e4fb4cc6c1f6d26f713544c4
beta <jiangyouxin@taou.com>
2016-06-24 23:43:27 +0800

    Fix bug

--- front_end/routes/components/CommonCard.js
+++ front_end/routes/components/CommonCard.js
@@ -8,10 +8,12 @@ export default ({user_id, avatar, txt, pic_id, new_item, children, onClick}) => 
     var has_children = children && children.length>0;
     return (
         <div className={`list-item${new_item ? ' new_item' : ''}`} onClick={onClick || ()=>{}}>
-            <img className='avatar' src={avatar} onClick={(e)=>{
-                e.stopPropagation();
-                hashHistory.push('detail/' + user_id);
-            }} />
+            <div className='avatar-container'>
+                <img className='avatar' src={avatar} onClick={(e)=>{
+                    e.stopPropagation();
+                    hashHistory.push('detail/' + user_id);
+                }} />
+            </div>
             { pic_id && <img className='picture' src={fconf.qiniu.site + pic_id + '-c167'} />}
             <div className="list-item-content">
                 <div className={`nickname${has_children ? '':' only'}`}>{ txt }</div>
