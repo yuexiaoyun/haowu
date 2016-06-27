3b3db24779c5aa4b9e2e4ef990020a4ea74277ab
beta <jiangyouxin@taou.com>
2016-06-27 21:47:56 +0800

    物记公众号关注的逻辑

--- front_end/routes/Detail/FollowLine.js
+++ front_end/routes/Detail/FollowLine.js
@@ -1,16 +1,29 @@
 import React from 'react'
 
+import PopupHelper from '../../utility/PopupHelper';
+
 import styles from './FollowLine.css'
 import CSSModules from 'react-css-modules';
 
-// TODO: 关注公众号的弹窗
+import qrcode from '../../files/qrcode.jpg';
+
 class FollowLine extends React.Component {
+    popup = () => {
+        PopupHelper.popup(
+            <div className='qr_dialog'>
+                <img className='qrcode' src={qrcode}/>
+                <div>长按识别图中二维码</div>
+                <div>即可关注“物记”公众号</div>
+            </div>
+        );
+    }
     shouldComponentUpdate(props) {
         return false;
     }
     render() {
         return (
-            <div styleName='root'>
+            <div styleName='root' onClick={this.popup}>
+                <img styleName='qrcode' src={qrcode}/>
                 <div>点击识别二维码，关注“物记”公众号</div>
                 <div>朋友互动不遗漏</div>
             </div>
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,7 +21,7 @@ module.exports = {
                 'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
             ]
         }, {
-            test: /\.png$/,
+            test: /\.png$|\.jpg$/,
             loader: "base64-image"
         }]
     },
