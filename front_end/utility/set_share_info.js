91932581f509c6b64fd64ce622bc9ee31b9b3522
beta <jiangyouxin@taou.com>
2016-07-02 17:23:51 +0800

    Fix bug

--- front_end/utility/set_share_info.js
+++ front_end/utility/set_share_info.js
@@ -4,7 +4,7 @@ import fconf from '../fconf'
 export default (s) => {
     wx.ready(()=>{
         var share_info = {
-            title: '点一下，这里有生活有趣的另一种可能 | 物记，好物有声',
+            title: '点一下，生活的另一种可能 | 物记，好物有声',
             desc: '图音好物分享社区，体验沉淀在物里的情感，在这里，慢下来',
             link: fconf.site + '/app/home',
             imgUrl: fconf.site + '/static/images/logo_80.png',
