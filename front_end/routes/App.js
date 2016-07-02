616c746833cedeaed9c7c1bb8ae90b7cc6530b84
beta <jiangyouxin@taou.com>
2016-07-02 18:30:46 +0800

    首页支持显示小黄点

--- front_end/routes/App.js
+++ front_end/routes/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import update from '../utility/update'
-import { Link, hashHistory } from 'react-router';
-import { connect } from 'react-redux';
+import { hashHistory } from 'react-router';
 import { createAction } from 'redux-actions';
-import { createStructuredSelector } from 'reselect'
 import wx from 'weixin-js-sdk';
+
+import styles from './App.css';
+import CSSModules from 'react-css-modules';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 class App extends React.Component {
     take_photo = () => {
@@ -21,27 +24,28 @@ class App extends React.Component {
         update('/api/update_badge');
     }
     render() {
-        var { children, location, badge } = this.props;
+        var { children, location, badge, feed_empty } = this.props;
         var current_tab = 0;
         return (
             <div>
                 {children}
                 <div style={{width: '100%', height: '49px', clear:'both', overflow:'hidden'}} />
-                <nav className="bar bar-tab">
-                    <span className={"bar-tab-item"} onClick={()=>{
+                <nav styleName="bar">
+                    <span styleName={"bar-tab-item"} onClick={()=>{
                         hashHistory.replace('/home/' + Date.parse(new Date()));
                     }}>
-                        <span className={"setting-icon image-btn_tabbar_home" +
+                        <span styleName='setting-icon' className={"image-btn_tabbar_home" +
                             (location.pathname.substring(0, 5)=='/home' ? '_selected' : '')} />
-                        { badge>0 && <span className="badge">{badge}</span> }
+                        { badge>0 && <span styleName="badge">{badge}</span>
+                        || feed_empty && <div styleName='new' /> }
                     </span>
-                    <span className={"bar-tab-item"} onClick={this.take_photo}>
-                        <span className="setting-icon image-btn_tabbar_photo"></span>
+                    <span styleName={"bar-tab-item"} onClick={this.take_photo}>
+                        <span styleName='setting-icon' className="image-btn_tabbar_photo"></span>
                     </span>
-                    <span className={"bar-tab-item"} onClick={()=>{
+                    <span styleName={"bar-tab-item"} onClick={()=>{
                         hashHistory.replace('/detail/' + window.user_id);
                     }}>
-                        <span className={"setting-icon image-btn_tabbar_me"
+                        <span styleName='setting-icon' className={"image-btn_tabbar_me"
                         + (location.pathname=='/detail/' + window.user_id ? '_selected' : '')} />
                     </span>
                 </nav>
@@ -50,6 +54,13 @@ class App extends React.Component {
     }
 }
 
-export default connect(({badge})=>({
-    badge: badge.count
-}))(App);
+var get_badge_count = state => state.badge.count;
+var get_new = state => state.feed_ids.length == 0;
+var mapStateToProps = createStructuredSelector({
+    badge: get_badge_count,
+    feed_empty: get_new
+});
+
+export default connect(mapStateToProps)(
+    CSSModules(App, styles)
+);
