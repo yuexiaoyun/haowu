002d6f96093ddd2dd944f324f642a5b406731de8
beta <jiangyouxin@taou.com>
2016-07-02 20:52:29 +0800

    物记，好物有声

--- utility/menu.js
+++ utility/menu.js
@@ -6,12 +6,25 @@ var menu = {
         type: "view",
         name: "进入物记",
         url: conf.site + '/app/home'
+    }, {
+        name: '我是新人',
+        sub_button: [{
+            type: 'media_id',
+            name: '意见反馈',
+            media_id: 'egOrCsB_0y-VIJsP2uYkaJoJfzMWGC8b0MVZrshD-3Q'
+        }, {
+            type: "view_limited",
+            name: "关于物记",
+            media_id: 'egOrCsB_0y-VIJsP2uYkaNmkmw-7YshZc7R8gzVCWzI'
+        }]
     }]
 };
 
 co.wrap(function *() {
     var result = yield* api.createMenu(menu);
     console.log(result);
+    var result = yield* api.getMaterials('image', 0, 1);
+    console.log(result.toString());
 })().then(function () {
 }).catch(function (err) {
     console.log(err);
