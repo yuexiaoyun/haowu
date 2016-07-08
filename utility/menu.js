8a106b2eea6fa37e3efd026319cb6c8d21f3a4e8
beta <jiangyouxin@taou.com>
2016-07-08 23:05:37 +0800

    改菜单

--- utility/menu.js
+++ utility/menu.js
@@ -7,11 +7,15 @@ var menu = {
         name: "进入物记",
         url: conf.site + '/app/home'
     }, {
+        type: 'media_id',
+        name: '送物换物',
+        media_id: 'egOrCsB_0y-VIJsP2uYkaNmPCJaDTTUQOL0QRqPPQ08'
+    }, {
         name: '关于',
         sub_button: [{
             type: 'media_id',
             name: '意见反馈',
-            media_id: 'egOrCsB_0y-VIJsP2uYkaJoJfzMWGC8b0MVZrshD-3Q'
+            media_id: 'egOrCsB_0y-VIJsP2uYkaDQZm3eehAhRmP8FZzPk0ys'
         }, {
             type: "view_limited",
             name: "关于物记",
@@ -23,7 +27,7 @@ var menu = {
 co.wrap(function *() {
     var result = yield* api.createMenu(menu);
     console.log(result);
-    var result = yield* api.getMaterials('image', 0, 1);
+    var result = yield* api.getMaterials('image', 0, 2);
     console.log(result.toString());
 })().then(function () {
 }).catch(function (err) {
